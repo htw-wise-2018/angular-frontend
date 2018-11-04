16e3aa78fb20940120852151494cf5bca1908fa0
Christoph Stach <christoph.stach@gmail.com>
2018-11-04 14:38:16 +0100

    remove unused files

--- src/app/floats-map/floats-map.module.ts
+++ src/app/floats-map/floats-map.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { FloatDetailsComponent } from './components/float-details/float-details.component';
 import { FilterButtonComponent } from './components/filter-button/filter-button.component';
-import { HtwLogoComponent } from './components/htw-logo/htw-logo.component';
 import { MapViewportComponent } from './components/map-viewport/map-viewport.component';
 import { MapComponent } from './components/map/map.component';
 
@@ -21,7 +20,6 @@ import { SwitchLayersComponent } from './components/bottom-sheets/switch-layers/
     MapViewportComponent,
     MapComponent,
     FilterButtonComponent,
-    HtwLogoComponent,
     FloatDetailsComponent,
     LayerButtonComponent,
     ButtonNavComponent,
